0af1419099bcf7c7796a7395fe73a8e16923b16b
QshoTigura <q.mehandjiiski2019@pmg-pernik.com>
2024-01-18 11:04:25 +0200

    Napraven e Scaffold na potrebitelq i promenen login i register

---
+++ SIMLovers/SIMLovers.Infrastructure/Data/Domain/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace SIMLovers.Infrastructure.Data.Domain
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(30)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(30)]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(30)]
+        public string Address { get; set; } = null!;
+    }
+}
